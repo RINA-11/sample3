155d77345d2ae807b57ba33d30c2bf41ee177287
RINA <74470264+RINA-11@users.noreply.github.com>
2021-02-02 14:57:35 +0000

    create_task3

---
+++ task3.rb
@@ -0,0 +1,24 @@
+puts "初めてのRuby"
+
+puts "Ruby"+"始めました"
+
+puts 3+3
+puts 3-3
+puts 3*3
+puts 3/3
+
+number=[1,2,3,4]
+
+num_array=number
+
+p num_array
+
+R={name:"R.K",birthday:1994,blood_type:"o"}
+
+def plus_ruby(word)
+  puts word+"ruby"
+end
+
+def plus_one(count)
+puts count+1
+end
