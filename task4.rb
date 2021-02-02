fa236ca911175c6d2e5882d564893937e8a87f6c
RINA <74470264+RINA-11@users.noreply.github.com>
2021-02-02 15:19:11 +0000

    create_task4

---
+++ task4.rb
@@ -0,0 +1,30 @@
+if n==3
+  puts "nは3です"
+end
+
+if n==3||n==4
+  puts "3、4のどちらかです"
+end 
+
+if n==3
+  puts "nは3です"
+elsif n==4
+  puts "nは4です"
+end 
+
+for i in 1..5 do
+  puts "こんにちは"
+end
+
+numbers=[1,2,3,4,5]
+numbers.each do |number|
+  puts number
+end 
+
+if n==2
+  puts "2です"
+elsif n==3
+    puts "3です"
+else
+    puts "それ以外です"
+end 
